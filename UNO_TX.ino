5bb8985dc8fb308e57c4963a9dab9baa89007707
Dogg3778 <102381583+Dogg3778@users.noreply.github.com>
2024-03-16 22:51:11 +0300

    Transmitter update 16/03
    Comments, LEDs

--- UNO_TX.ino
+++ UNO_TX.ino
@@ -1,43 +1,45 @@
-/*
-Победа над nRF24L01: на три шага ближе, передатчик
-https://habr.com/ru/post/476716/
-*/
-
 #include <SPI.h>
 #include <RF24.h>
-#include<time.h>
-RF24 radio(9, 10); // порты D9, D10: CSN CE
-const uint32_t pipe = 111156789; // адрес рабочей трубы;
-
-byte data;
-
+//#include <LiquidCrystal.h>                        // задел на будущее
+#include<time.h>                                  // подключение библиотек
+RF24 radio(9, 10);                                // порты D9, D10: CSN CE
+const uint32_t pipe = 111156789;                  // адрес рабочей трубы
+byte data;                                        // переменная, отправляемая в эфир
+//LiquidCrystal lcd(0, 1, 5, 6, 7, 8);              //=lcd(13, 12, 11, 10, 9, 8) задел на будущее
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Transmitter ON");
+  //Serial.begin(115200);                           // (отладка) настройка COM-порта
+  //Serial.println("Transmitter ON");               // (отладка) отправка сообщения о запуске в COM-порт
 
-  radio.begin();                // инициализация
-  delay(2000);
-  radio.setDataRate(RF24_1MBPS); // скорость обмена данными RF24_1MBPS или RF24_2MBPS
-  radio.setCRCLength(RF24_CRC_8); // размер контрольной суммы 8 bit или 16 bit
-  radio.setPALevel(RF24_PA_MAX); // уровень питания усилителя RF24_PA_MIN, RF24_PA_LOW, RF24_PA_HIGH and RF24_PA_MAX
-  radio.setChannel(0x6f);         // установка канала
-  radio.setAutoAck(false);       // автоответ
-  radio.powerUp();               // включение или пониженное потребление powerDown - powerUp
-  radio.stopListening();  //радиоэфир не слушаем, только передача
-  radio.openWritingPipe(pipe);   // открыть трубу на отправку
+  radio.begin();                                  // инициализация
+  delay(2000);                                    // ожидание стабилизации работы радиомодуля
+  radio.setDataRate(RF24_1MBPS);                  // скорость обмена данными
+  radio.setCRCLength(RF24_CRC_8);                 // размер контрольной суммы
+  radio.setPALevel(RF24_PA_MIN);                  // уровень питания усилителя
+  radio.setChannel(0x6f);                         // установка канала
+  radio.setAutoAck(false);                        // отключение автоответа
+  radio.powerUp();                                // включение
+  radio.stopListening();                          // переход в режим передачи
+  radio.openWritingPipe(pipe);                    // открыть трубу на отправку
+  pinMode(2, OUTPUT);                             // красный диод
+  pinMode(3, OUTPUT);                             // желтый диод
+  pinMode(4, OUTPUT);                             // зеленый диод
+  digitalWrite(2, 0);
+  digitalWrite(3, 0);
+  digitalWrite(4, 0);
+  //lcd.begin(16,2);                                // задел на будущее
+  //lcd.print("TEST");                              // задел на будущее
 }
 
 void loop() {
-  /*if (Serial.available()){
-    data = Serial.parseInt();
-  }*/
-  for(int i {1}; i<4; i++){
-    unsigned long t = millis();
-    data = i;
-    while (millis() < t+5000){
-      radio.write(&data, 1);
-      Serial.println("data= " + String(data));
+  for(int i {1}; i<4; i++){                       // цикл с перебором i от 1 до 3 (1 - красный, 2 - желтый, 3 - зеленый)
+    unsigned long t = millis();                   // замер текущего времени
+    data = i;                                     // присвоение текущего сигнала переменной отправки
+    while (millis() < t+5000){                    // проверка: прошло ли 5 секунд с момента замера времени, если нет - отправка значения, если да - значение меняется
+      radio.write(&data, 1);                      // отправка значения в эфир
+      digitalWrite(i+1, 1);                       // включение светодиода
+      //Serial.println(String(data));               // (отладка) отправка значения в COM-порт
     };
+    digitalWrite(i+1, 0);                         // отключение светодиода
   };
 }
