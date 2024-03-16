f84341d8836c9b283cf9866d1542c102718c6faf
Dogg3778 <102381583+Dogg3778@users.noreply.github.com>
2024-03-16 22:50:01 +0300

    Receiver code update 16/03
    Comments, buzzer, input filter

--- NANO_RX.ino
+++ NANO_RX.ino
@@ -1,34 +1,36 @@
-/*
-Победа над nRF24L01: на три шага ближе, приемник
-https://habr.com/ru/post/476716/
-*/
 #include <SPI.h>
 #include "nRF24L01.h"
-#include "RF24.h"
+#include "RF24.h"                     // подключение библиотек
 
-RF24 radio(9, 10); // порты D9, D10: CSN CE
-
-const uint32_t pipe = 111156789; // адрес рабочей трубы;
-byte data[1];
-int scn;  //счетчик циклов прослушивания эфира
-int sg;  //счетчик числа принятых пакетов с передатчика
+RF24 radio(9, 10);                    // порты D9, D10: CSN CE
+const uint32_t pipe = 111156789;      // адрес рабочей трубы;
+byte data[1];                         // переменная для записи принимаемых данных
+String prev = "";                     // предыдущее значение
+int time = 0;
 
 void setup() {
-  Serial.begin(9600);
-
-  radio.begin();  // инициализация
-  delay(2000);
-  radio.setDataRate(RF24_1MBPS); // скорость обмена данными RF24_1MBPS или RF24_2MBPS
-  radio.setCRCLength(RF24_CRC_8); // размер контрольной суммы 8 bit или 16 bit
-  radio.setChannel(0x6f);         // установка канала
-  radio.setAutoAck(false);       // автоответ
-  radio.openReadingPipe(1, pipe); // открыть трубу на приём
-  radio.startListening();        // приём
+  Serial.begin(9600);                 // настройка COM-порта
+  radio.begin();                      // инициализация
+  delay(2000);                        // ожидание стабилизации работы радиомодуля
+  radio.setDataRate(RF24_1MBPS);      // скорость обмена данными
+  radio.setCRCLength(RF24_CRC_8);     // размер контрольной суммы
+  radio.setChannel(0x6f);             // установка канала
+  radio.setAutoAck(false);            // отключение автоответф
+  radio.openReadingPipe(1, pipe);     // открыть трубу на приём
+  radio.startListening();             // приём
+  pinMode(3, OUTPUT);                 // перевод 3го пина в режим вывода
 }
 
 void loop() {
-  radio.read(data, 1);
-  if (data[0] > 0){
-    Serial.println(String(data[0]));
+  radio.read(data, 1);                                // чтение эфира
+  if ((data[0] > 0)&&(String(data[0]) != prev)){      // проверка: пришёл ли новый сигнал
+    Serial.println(String(data[0]));                  // отправка в COM-порт принятых радиосигналов
+    prev = String(data[0]);                           // запись предыдущего сигнала в переменную
+    time = millis();                                  // установка точки отсчета времени
+  }
+  if ((millis()<time+200)){                           // таймер на 200мс для пищалки
+    analogWrite(3, 100);                              // подача сигнала на пищалку
+  } else {
+    analogWrite(3, 0);                                // отключение пищалки
   }
 }
